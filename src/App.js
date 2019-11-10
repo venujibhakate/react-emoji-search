e1ad4bbfab0b11ec81fa5b20d95af6d9a0e2a71c
Venu <you@example.com>
2019-11-10 20:12:51 +0530

    I used the filter() in the code and input part has completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import React,{Component} from 'react';
-// import "./App.css";
-import Filter from "./filter";
-import Search from './search'
 import Header from './Header';
+import json from "./emojiList.json";
+import "./App.css";
 
-
+function searchingFor(term){
+    return function(x){
+        return x.keywords.toLowerCase().includes(term.toLowerCase())
+         || x.title.toLowerCase().includes(term.toLowerCase())
+    }
+}
 
 class App extends Component {
+    constructor(props){
+        super(props);
+            this.state = {
+                json : json,
+                term: "",
+        }
+        this.searchHandler = this.searchHandler.bind(this);
+    }
+
+    searchHandler(event){
+        this.setState({term: event.target.value})
+    }
     render() {
+        const {term,json} = this.state;
         return (
             <div className="App">
             <Header/>
-            <Search/>
-            <Filter/>
+            <form>
+                <input type="text"
+                onChange={this.searchHandler}
+            />
+            </form>
+            {
+            
+             json.filter(searchingFor(term)).slice(0,20).map(post =>
+                <div  className="filter-emoji">
+                <h1>
+                {post.symbol}
+                {post.title}
+                </h1>
+            </div>
+             )
+             }
             </div>
         );
     }
 }
 export default App;
+
+
--- src/Greet.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-const Greet = (props) => {
-    console.log(props)
-    return <h1>Hello {props.name}</h1>
-}
-export default Greet
